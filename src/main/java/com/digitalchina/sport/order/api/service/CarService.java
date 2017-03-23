acc2defb5d83b35ed7f52dd797aa4b8fc853998b
rensq <1002367466@qq.com>
2017-03-23 10:42:42 +0800

    我的停车

--- src/main/java/com/digitalchina/sport/order/api/service/CarService.java
+++ src/main/java/com/digitalchina/sport/order/api/service/CarService.java
@@ -22,33 +22,32 @@ public class CarService {
     };
 
     public Map<String,Object> insertCar(Map<String,Object> map){
+        Map<String,Object> param = map;//添加车辆的参数
+
         Map<String,Object> reMap = new HashMap<String, Object>();
-        int count = carDao.getCount(map);
+        int count = carDao.getCount(map);//判断车辆是否被绑定
         if (count>0) {
-            reMap.put("userId",map.get("userId"));
-            reMap.put("carNumber",map.get("carNumber"));
             reMap.put("returnKey","false");
             reMap.put("returnMessage","该车辆已绑定!");
-        }else if (count>=5) {
-            reMap.put("userId",map.get("userId"));
-            reMap.put("carNumber",map.get("carNumber"));
+        }
+        map.remove("carNumber");
+        int count2 = carDao.getCount(map);//判断最多绑定5个车牌号
+        if (count2>=5) {
             reMap.put("returnKey","false");
             reMap.put("returnMessage","同一账号最多可绑定5个车牌号!");
         }else {
             String id = UUIDUtil.generateUUID();//uuid生成32位随机数id
-            map.put("id",id);
-            if (carDao.insertCar(map)>0){
-                reMap.put("userId",map.get("userId"));
-                reMap.put("carNumber",map.get("carNumber"));
+            param.put("id",id);
+            if (carDao.insertCar(param)>0){
                 reMap.put("returnKey","true");
                 reMap.put("returnMessage","绑定成功!");
             }else {
-                reMap.put("userId",map.get("userId"));
-                reMap.put("carNumber",map.get("carNumber"));
                 reMap.put("returnKey","true");
                 reMap.put("returnMessage","绑定失败!");
             }
         }
+        reMap.put("userId",param.get("userId"));
+        reMap.put("carNumber",param.get("carNumber"));
         return reMap;
     };
 
