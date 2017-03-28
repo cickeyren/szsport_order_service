7940d3e38b1116575c9fe41d981a75dd2d543332
wanggw <350113733@qq.com>
2017-03-28 13:54:28 +0800

    更新支付宝退款接口及订单修改

--- src/main/java/com/digitalchina/pay/alipay/AlipayPayController.java
+++ src/main/java/com/digitalchina/pay/alipay/AlipayPayController.java
@@ -100,28 +100,30 @@ public class AlipayPayController {
             return;
         }
         //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
+        //判断回调参数是否包含refund_status字段
+        if (params.containsKey("refund_status")){
+                logger.error("========================支付宝退款成功=================");
+        }else {
+            if(com.digitalchina.pay.alipay.util.AlipayNotify.verify(params,tradeVo.getPartnerId())){
+                //////////////////////////////////////////////////////////////////////////////////////////
+                //请在这里加上商户的业务逻辑程序代码
 
-        if(com.digitalchina.pay.alipay.util.AlipayNotify.verify(params,tradeVo.getPartnerId())){//验证成功
-            //////////////////////////////////////////////////////////////////////////////////////////
-            //请在这里加上商户的业务逻辑程序代码
+                //——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
 
-            //——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
 
-            if(trade_status.equals("TRADE_FINISHED")){
-                //判断该笔订单是否在商户网站中已经做过处理
-                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-                //如果有做过处理，不执行商户的业务程序
 
-                //注意：
-                //退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
-                //请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
-            } else if (trade_status.equals("TRADE_SUCCESS")){
-                //判断该笔订单是否在商户网站中已经做过处理
-                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-                //如果有做过处理，不执行商户的业务程序
-                if (refund_status.equals("REFUND_SUCCESS")){
-                    logger.error("========================支付宝退款成功=================");
-                }else {
+                if(trade_status.equals("TRADE_FINISHED")){
+                    //判断该笔订单是否在商户网站中已经做过处理
+                    //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                    //如果有做过处理，不执行商户的业务程序
+
+                    //注意：
+                    //退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
+                    //请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
+                } else if (trade_status.equals("TRADE_SUCCESS")){
+                    //判断该笔订单是否在商户网站中已经做过处理
+                    //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                    //如果有做过处理，不执行商户的业务程序
                     if( null != tradeVo) {
                         tradeVo.setOutTradeNo(out_trade_no);
                         tradeVo.setTradeNo(trade_no);
@@ -173,19 +175,20 @@ public class AlipayPayController {
                     //付款完成后，支付宝系统发送该交易状态通知
                     //请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
 
-                }
-            }else if (refund_status.equals("REFUND_SUCCESS")){
+
+                }else if (refund_status.equals("REFUND_SUCCESS")){
                     logger.error("========================支付宝退款成功=================");
+                }
+
+                //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+                out.println("success");	//请不要修改或删除
+
+                //////////////////////////////////////////////////////////////////////////////////////////
+            }else{//验证失败
+                logger.error("========================支付宝回调验签失败=================");
+                out.println("fail");
             }
-
-            //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
-
-            out.println("success");	//请不要修改或删除
-
-            //////////////////////////////////////////////////////////////////////////////////////////
-        }else{//验证失败
-            logger.error("========================支付宝回调验签失败=================");
-            out.println("fail");
         }
         out.flush();
         out.close();
